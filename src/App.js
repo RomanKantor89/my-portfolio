d853ef0f854cf11cfd0ffedcb4f99aef48fb62b8
Roman Kantor <Roman.kantor89@gmail.com>
2020-09-13 21:29:41 -0400

    404 page
    also added transition to nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch} from "react-router-dom";
 import { faHome, faUser, faIdCard, faSuitcase, faBlog, faEnvelope, faBars } from "@fortawesome/free-solid-svg-icons";
 import { faGithubSquare } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CSSTransition } from 'react-transition-group';
 import Home from './components/Home/Home'
 import AboutMe from './components/About_me/AboutMe'
 import Resume from './components/Resume/Resume'
@@ -15,43 +16,56 @@ import NotFound from './components/Not_found/NotFound'
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.updateMenuState = this.updateMenuState.bind(this);
+
+  }
   state = {
     mobileToggle: false,
     isActive: false,
+  }
+
+  updateMenuState(){
+    this.setState({mobileToggle: !this.state.mobileToggle})
   }
   
   render() {
     return(
       <div className="container">
+
         {/* mobile menu */}
         <div className="mobile-nav">
-          <a className="mobile-toggle" onClick={(e) => this.setState({mobileToggle: !this.state.mobileToggle})}>
+          <a className="mobile-toggle" onClick={this.updateMenuState}>
             <FontAwesomeIcon icon={faBars}/>  
           </a>
           <h2>ROMAN KANTOR</h2>
         </div>
         {/* main menu */}
-        <div className={this.state.mobileToggle ? "main-nav-on" : "main-nav"}>
-          <div className="head-shot-section"> 
-            <img className="HeadShotImage" src={require('../src/images/RomanHeadShotCroped.jpg')} alt="Original"/>
-            <h2>ROMAN KANTOR</h2>
+        <CSSTransition in={this.state.mobileToggle} timeout={500} classNames="main-nav" > 
+
+          <div className={this.state.mobileToggle ? "main-nav-on" : "main-nav"}>
+            <div className="head-shot-section"> 
+              <img className="HeadShotImage" src={require('../src/images/RomanHeadShotCroped.jpg')} alt="Original"/>
+              <h2>ROMAN KANTOR</h2>
+            </div>
+            <div className="main-menu-links" id="NavLinks" >
+              <Navbar />
+            </div>
+            <div className="main-menu-links">
+              <p id="githubIcon">  
+              <Nav.Link href="https://github.com/RomanKantor89" target="_blank">
+                <FontAwesomeIcon icon={faGithubSquare}/>
+              </Nav.Link>
+              </p>
+            </div>
+            <div className="main-menu-links">
+              <p>2020 © Roman Kantor.<br></br>
+                  All Rights Reserved.
+              </p>
+            </div>
           </div>
-          <div className="main-menu-links" id="NavLinks" >
-            <Navbar />
-          </div>
-          <div className="main-menu-links">
-            <p id="githubIcon">  
-            <Nav.Link href="https://github.com/RomanKantor89" target="_blank">
-              <FontAwesomeIcon icon={faGithubSquare}/>
-            </Nav.Link>
-            </p>
-          </div>
-          <div className="main-menu-links">
-            <p>2020 © Roman Kantor.<br></br>
-                All Rights Reserved.
-            </p>
-          </div>
-        </div>
+        </CSSTransition >
 
         <div className="content">
           <Switch>
--- src/components/Not_found/NotFound.js
+++ src/components/Not_found/NotFound.js
@@ -1,13 +1,95 @@
 import React, { Component } from 'react';
+import Particles from 'react-tsparticles'
+import Nav from 'react-bootstrap/Nav';
 import '../../App.css';
 import './NotFound.css';
 
 class NotFound extends Component {
   render(){
     return(
-      <div>
-        Not Found Component
-      </div>
+    <div className="page">        
+      <Particles className="particles"
+      options={{
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 2,
+            },
+            repulse: {
+              distance: 150,
+              duration: 0.3,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "rgb(143, 12, 0)",
+          },
+          links: {
+            color: "rgb(87, 172, 230)",
+            distance: 210,
+            enable: true,
+            opacity: 0.1,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 1000,
+            },
+            value: 100,
+          },
+          opacity: {
+            value: 0.3,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 4,
+          },
+        },
+       
+      }}
+    />
+    <div className="main-not-found">
+      <h1 className="lost"> You got lost.. its time to go.. </h1>
+      <h1 className="home-link"> <Nav.Link href="/"> HOME </Nav.Link></h1>
+      <img className="lost-image" src={require('../../images/lost.png')} alt="Original"/>
+    </div>
+
+   </div>  
     )
   }
 }
